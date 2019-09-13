3e4a48dad842e66d496932981e877af473057ba4
Vineet Reddy <hotshot2797@gmail.com>
2019-09-13 20:45:42 +0530

    Works for 4 gram

--- test.py
+++ test.py
@@ -28,12 +28,12 @@ words = words.replace('“', '')
 words = words.split(" ")
 
 
-what = Counter(ngrams(words, 3))
+
 
 from itertools import permutations
 import random
 
-def find_highest_freq(word_list):
+def find_highest_freq(word_list, ngram):
     def partial_match(key, d):
         for k, v in d.items():
             if all(k1 == k2 or k2 is None  for k1, k2 in zip(k, key)):
@@ -41,22 +41,31 @@ def find_highest_freq(word_list):
     mykeys = list(partial_match((word_list), what))
     subdict = {x: what[x] for x in mykeys if x in what}
     # print(subdict)
-    return(max(subdict, key=subdict.get)[2])
+    return(max(subdict, key=subdict.get)[3])
 
-starter = "I"
-starter_1 = "am"
-gibberish = []
-gibberish.append(starter)
-gibberish.append(starter_1)
-gibberish.append(find_highest_freq((starter,starter_1,None)))
-# print(gibberish[-1])
-# print("Im here")
-# print(gibberish)
-for i in range(0,10):
-    gibberish.append(find_highest_freq((gibberish[-2],gibberish[-1], None)))
-    # starter = find_highest_freq((starter,None))
-    # starter_1 = find_highest_freq((starter_1, None, None))
-print(gibberish)
+def final(n):
+
+    starter = "I"
+    starter_1 = "am"
+    starter_2 = "a"
+    gibberish = []
+    gibberish.append(starter)
+    gibberish.append(starter_1)
+    gibberish.append(starter_2)
+    gibberish.append(find_highest_freq((starter,starter_1,starter_2,None),n))
+    # print(gibberish)
+    # print(gibberish[-1])
+    # print("Im here")
+    # print(gibberish)
+    for i in range(0,10):
+        gibberish.append(find_highest_freq((gibberish[-3],gibberish[-2],gibberish[-1], None),n))
+        # starter = find_highest_freq((starter,None))
+        # starter_1 = find_highest_freq((starter_1, None, None))
+    print(gibberish)
+
+n = 4
+what = Counter(ngrams(words, n))
+final(n)
 
 # print(find_highest_freq(("I", )))
 
